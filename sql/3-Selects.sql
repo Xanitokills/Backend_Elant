bee8f7ac4c6ae6cb0835896684b5a12a083dabe3
Zerguiter001 <JorgeJesus_30@outlook.com>
2025-03-28 14:04:17 -0500

    Se organiza plan de base de datos
    Se organiza plan de base de datos

---
+++ sql/3-Selects.sql
@@ -0,0 +1,7 @@
+-----------------
+
+SELECT * FROM MAE_USUARIO WHERE CORREO = 'test@example.com';
+
+SELECT CONTRASENA_HASH FROM MAE_USUARIO WHERE CORREO = 'test@example.com';
+
+SELECT CONTRASENA_HASH FROM MAE_USUARIO WHERE CORREO = 'test@example.com';
