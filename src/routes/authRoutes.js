17bcfa65029b06de9515c6fea64a0cf2c3af0932
Zerguiter001 <JorgeJesus_30@outlook.com>
2025-04-15 14:20:36 -0500

    authroutes
    authroutes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -24,7 +24,7 @@ router.get("/users", authMiddleware, checkPermission("Usuarios"), getAllUsers);
 router.put("/users/:id", authMiddleware, checkPermission("Usuarios"), updateUser);
 router.delete("/users/:id", authMiddleware, checkPermission("Usuarios"), deleteUser);
 router.get("/movements", authMiddleware, checkPermission("Movimientos"), getAllMovements);
-router.get("/get-login-images", authMiddleware, checkPermission("ConfigLogin"), getLoginImages);
+router.get("/get-login-images", getLoginImages); // Ruta pública
 router.post("/upload-login-images", authMiddleware, checkPermission("ConfigLogin"), upload.single("image"), uploadImage);
 router.delete("/delete-login-image/:imageId", authMiddleware, checkPermission("ConfigLogin"), deleteLoginImage);
 router.put("/auth/change-password", authMiddleware, changeAuthenticatedUserPassword);
